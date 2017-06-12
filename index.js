6b4986632e4784956bef3d2c1bbd42e1dd7ec7f8
Pierre Laferriere <plaferriere@zipitwireless.com>
2017-06-12 12:58:48 -0400

    Allow animation on state update without clicking

--- index.js
+++ index.js
@@ -125,6 +125,11 @@ var MaterialSwitch = React.createClass({
     });
   },
 
+  componentWillReceiveProps(props) {
+    if (props.active != this.state.state && props.active) this.activate();
+    else if (props.active != this.state.state && !props.active) this.deactivate();
+  },
+
   onSwipe(currentPosition, startingPosition, onChange, onTerminate) {
     if (currentPosition-startingPosition >= 0) {
       if (currentPosition-startingPosition > this.state.width/2 || startingPosition == this.state.width) {
